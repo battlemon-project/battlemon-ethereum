2c0e4af78498a986313bc3746e5f3b066f351330
ChzenChzen <fedorovdanila@me.com>
2023-04-30 16:01:04 +0700

    add logging to functions

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,12 @@
 use crate::config::{DatabaseConfig, MainConfig};
 use eyre::{Result, WrapErr};
 use sqlx::{postgres::PgPoolOptions, PgPool};
+use tracing::instrument;
 
 pub struct App {}
 
 impl App {
+    #[instrument(name = "Building Application", skip_all)]
     pub async fn build(config: MainConfig) -> Result<Self> {
         let pool = get_db_pool(&config.db);
         sqlx::migrate!()
@@ -16,6 +18,7 @@ impl App {
     }
 }
 
+#[instrument(name = "Getting database pool", skip_all)]
 pub fn get_db_pool(config: &DatabaseConfig) -> PgPool {
     PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
