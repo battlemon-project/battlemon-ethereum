23867d8e8152b4e7532cc8f99f5fb790a81a92ca
ChzenChzen <fedorovdanila@me.com>
2023-05-05 20:33:13 +0700

    add jwt token to auth handler

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,3 +1,4 @@
+use crate::jwt::Jwt;
 use axum::{
     extract::{Json, State},
     http::StatusCode,
@@ -5,9 +6,8 @@ use axum::{
 };
 use ethers::prelude::{Address, Signature, SignatureError};
 use eyre::{Report, Result, WrapErr};
-use jsonwebtoken::EncodingKey;
 use serde::Deserialize;
-use sqlx::PgPool;
+use sqlx::{PgPool, Postgres, Transaction};
 use thiserror::Error;
 use tracing::instrument;
 use uuid::Uuid;
@@ -41,13 +41,15 @@ impl TryFrom<Payload> for ValidatedPayload {
 
 #[instrument(name = "Web3 auth", skip_all, err(Debug))]
 pub async fn web3_auth(
+    State(jwt): State<Jwt>,
     State(db_pool): State<PgPool>,
-    State(key): State<EncodingKey>,
     Json(payload): Json<Payload>,
-) -> Result<(), AuthError> {
+) -> Result<String, AuthError> {
     let ValidatedPayload { user_id, signature } =
         payload.try_into().map_err(AuthError::Validation)?;
-    let nonce = get_user_nonce_db(&user_id, &db_pool)
+
+    let user_id_string = format!("0x{user_id:x}");
+    let nonce = get_user_nonce_db(&user_id_string, &db_pool)
         .await
         .wrap_err("Failed to get nonce for user")?;
 
@@ -55,21 +57,40 @@ pub async fn web3_auth(
         .verify(nonce.to_string(), user_id)
         .map_err(AuthError::SignatureVerification)?;
 
-    Ok(())
+    let jwt_token = jwt.encode()?;
+    let mut tx = db_pool
+        .begin()
+        .await
+        .wrap_err("Failed to start transaction")?;
+    store_jwt_token_db(&user_id_string, &jwt_token, &mut tx)
+        .await
+        .wrap_err("Failed to store jwt token into database")?;
+    tx.commit().await.wrap_err("Failed to commit transaction")?;
+
+    Ok(jwt_token)
 }
 
 #[instrument(name = "Get nonce for user from database", skip(db_pool))]
-async fn get_user_nonce_db(user_id: &Address, db_pool: &PgPool) -> Result<Uuid, sqlx::Error> {
+async fn get_user_nonce_db(user_id: &str, db_pool: &PgPool) -> Result<Uuid, sqlx::Error> {
     let ret = sqlx::query!(
         r#"
         select nonce from users where user_id = $1
        "#,
-        format!("0x{user_id:x}")
+        user_id
     )
     .fetch_one(db_pool)
     .await?;
 
     Ok(ret.nonce)
+}
+
+#[instrument(name = "Store JWT token in database", skip(tx, jwt_token))]
+async fn store_jwt_token_db(
+    user_id: &str,
+    jwt_token: &str,
+    tx: &mut Transaction<'_, Postgres>,
+) -> Result<(), sqlx::Error> {
+    todo!()
 }
 
 #[derive(Error, Debug)]
