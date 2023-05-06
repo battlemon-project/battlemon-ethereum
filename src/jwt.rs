065ce5c228ee1c61916a770bcff566933935817f
ChzenChzen <fedorovdanila@me.com>
2023-05-06 17:32:14 +0700

    - add decoding key into Jwt - add decode method

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,20 +1,22 @@
+use crate::routes::Claims;
 use chrono::{Duration, Utc};
 use eyre::{Result, WrapErr};
-use jsonwebtoken::{EncodingKey, Header};
-use serde::{Deserialize, Serialize};
+use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, Validation};
 
 #[derive(Clone)]
 pub struct Jwt {
-    key: EncodingKey,
+    encoding_key: EncodingKey,
+    decoding_key: DecodingKey,
 }
 
 impl Jwt {
     pub fn new(key: &str) -> Self {
         Self {
-            key: EncodingKey::from_secret(key.as_bytes()),
+            encoding_key: EncodingKey::from_secret(key.as_bytes()),
+            decoding_key: DecodingKey::from_secret(key.as_bytes()),
         }
     }
-    
+
     pub fn encode(&self) -> Result<String> {
         let now = Utc::now();
         let expires_at = now + Duration::hours(1);
@@ -23,13 +25,17 @@ impl Jwt {
             iat: now.timestamp() as usize,
         };
 
-        jsonwebtoken::encode(&Header::default(), &claims, &self.key)
+        jsonwebtoken::encode(&Header::default(), &claims, &self.encoding_key)
             .wrap_err("Failed to encode claims")
     }
-}
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Claims {
-    pub exp: usize,
-    pub iat: usize,
+    pub fn decode(&self, token: &str) -> Result<Claims> {
+        jsonwebtoken::decode(
+            token,
+            &self.decoding_key,
+            &Validation::new(Algorithm::HS256),
+        )
+        .map(|decoded| decoded.claims)
+        .wrap_err("Failed to decode token")
+    }
 }
