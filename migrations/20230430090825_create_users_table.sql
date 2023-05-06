153564db61d150ecfaf796654d247ad1322c55bc
ChzenChzen <fedorovdanila@me.com>
2023-05-06 17:43:10 +0700

    update scheme

--- migrations/20230430090825_create_users_table.sql
+++ migrations/20230430090825_create_users_table.sql
@@ -1,6 +1,6 @@
 create table users
 (
-    user_id    varchar(42) primary key,
-    nonce      uuid not null,
-    jwt_token text
+    user_id   varchar(42) primary key,
+    nonce     uuid not null,
+    jwt_token text not null
 )
