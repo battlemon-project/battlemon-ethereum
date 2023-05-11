dd2d09fee88c7a7c95e68bf8bbf52bd11267fd1d
ChzenChzen <fedorovdanila@me.com>
2023-05-11 22:45:07 +0700

    update return type for increasing usability

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -15,17 +15,19 @@ pub fn build_subscriber<Sink>(
     name: String,
     env_filter: String,
     sink: Sink,
-) -> impl Subscriber + Send + Sync
+) -> Box<dyn Subscriber + Send + Sync>
 where
     Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
 {
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
     let formatting_layer = BunyanFormattingLayer::new(name, sink);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
+    Box::new(
+        Registry::default()
+            .with(env_filter)
+            .with(JsonStorageLayer)
+            .with(formatting_layer),
+    )
 }
 
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) -> Result<()> {
