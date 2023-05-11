be81b36c50535dfd056dcaaea3b2c3ebb733b7bb
ChzenChzen <fedorovdanila@me.com>
2023-05-12 01:04:55 +0700

    add test for many times calling get nonce

--- tests/nonce.rs
+++ tests/nonce.rs
@@ -1,5 +1,6 @@
 mod helpers;
 
+use std::collections::HashSet;
 use uuid::Uuid;
 
 use helpers::spawn_app;
@@ -28,4 +29,42 @@ async fn nonce_for_new_user_works_correctly() {
         .json()
         .await
         .expect("Failed to deserialize body into `Uuid`");
+}
+
+#[tokio::test]
+async fn nonce_for_user_works_correctly_many_times() {
+    let app = spawn_app().await;
+    let user_id = app.test_user.id();
+    let mut nonces = HashSet::new();
+    let expected_quantity_of_nonce = 10;
+    for _ in 0..expected_quantity_of_nonce {
+        let response = app
+            .get(&format!("users/{user_id}/nonce"), None)
+            .await
+            .unwrap();
+
+        let status = response.status();
+        assert!(
+            status.is_success(),
+            r#"
+        Failed to get nonce for user `{user_id}`.
+        The status of response is `{status}`.
+        Error body: {}
+        "#,
+            response.text().await.unwrap()
+        );
+
+        let nonce: Uuid = response
+            .json()
+            .await
+            .expect("Failed to deserialize body into `Uuid`");
+
+        nonces.insert(nonce);
+    }
+
+    assert_eq!(
+        expected_quantity_of_nonce,
+        nonces.len(),
+        "Collision of nonces occurred"
+    );
 }
