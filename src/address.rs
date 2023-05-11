18012496f20d5765ad01778da646376bf5d9838a
ChzenChzen <fedorovdanila@me.com>
2023-05-12 00:19:43 +0700

    add test for `ToHex`

--- src/address.rs
+++ src/address.rs
@@ -1,13 +1,26 @@
-use std::fmt::LowerHex;
 use ethers::addressbook::Address;
+use std::fmt::LowerHex;
 
 pub trait ToHex
 where
     Self: LowerHex,
 {
     fn to_hex(&self) -> String {
-        format!("0x{:x}", self)
+        format!("0x{:x}", self).to_lowercase()
     }
 }
 
 impl ToHex for Address {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn to_hex_works() {
+        let expected = "0x4675c7e5baafbffbca748158becba61ef3b0a263";
+        let actual: Address = expected.parse().unwrap();
+
+        assert_eq!(expected, actual.to_hex());
+    }
+}
