f97822734e655878592a88601767ba112160961d
ChzenChzen <fedorovdanila@me.com>
2023-05-12 02:02:43 +0700

    add database configuration

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -7,8 +7,11 @@ use battlemon_ethereum::{
 use ethers::prelude::Address;
 use eyre::{Result, WrapErr};
 
+use battlemon_ethereum::config::DatabaseConfig;
 use once_cell::sync::Lazy;
 use reqwest::{Client, Method, RequestBuilder, Response};
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -38,7 +41,7 @@ impl TestUser {
 pub struct TestApp {
     pub address: String,
     // pub db_name: String,
-    // pub db_pool: PgPool,
+    pub db_pool: PgPool,
     pub test_user: TestUser,
 }
 
@@ -63,39 +66,45 @@ impl TestApp {
             .await
             .wrap_err("Failed to make request")
     }
-
-    pub fn test_user_id(&self) -> String {
-        self.test_user.id()
-    }
 }
 
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
     let mut config = load_config().expect("Failed to read configuration");
-    // cfg.database.database_name = Uuid::new_v4().to_string();
+    config.db.db_name = Uuid::new_v4().to_string();
+    let db_pool = configure_database(&config.db).await;
     config.app.port = 0;
     let app = App::build(config)
         .await
         .expect("Failed to build app for testing");
     let address = format!("127.0.0.1:{}", app.port());
     tokio::spawn(app.run_until_stopped());
-    // configure_database(&config.database).await;
-    // let application = Application::build(config.clone())
-    //     .await
-    //     .expect("Failed to build application");
-    // let address = format!("127.0.0.1:{}", application.port());
-    // actix_web::rt::spawn(application.run_until_stopped());
-    // let ret = TestApp {
-    //     address,
-    //     db_name: config.database.database_name.clone(),
-    //     db_pool: get_connection_pool(&config.database),
-    //     test_user: TestUser::generate(),
-    // };
-    // ret.test_user.store(&ret.db_pool).await;
-    //
-    // ret
+
     TestApp {
         test_user: TestUser::random(),
+        db_pool,
         address,
     }
+}
+
+pub async fn configure_database(config: &DatabaseConfig) -> PgPool {
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.db_name).as_str())
+        .await
+        .expect("Failed to create database");
+
+    let db_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    sqlx::migrate!("./migrations")
+        .run(&db_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    db_pool
 }
